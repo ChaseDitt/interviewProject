614ac53e5b550d587d88b0b7a25afff79f1e70a1
Chase Dittmer <91153102+ChaseDitt@users.noreply.github.com>
2024-05-22 19:32:18 -0500

    Updated routes to look better and added redirecting
    Using Device it authenticates the user before redirecting to the home page. I also updated the routes as the generated ones in my opinion are gross and these look nicer

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,29 @@
 Rails.application.routes.draw do
-  get 'pages/login'
-  get 'pages/create'
-  get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+  
+  # Devise routes for User authentication
   devise_for :users
+
+  # Routes for Logging in (also set root to the login itself)
+  root to: 'pages#login'
+  get 'login', to: 'pages#login'
+  get 'sign_up', to: 'pages#create'
+  get 'home', to: 'pages#home'
+
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Redirect after sign in or sign up
+  devise_scope :user do
+    authenticated :user do
+      root to: 'pages#home', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root to: 'pages#login', as: :unauthenticated_root
+    end
+  end
+
 end
